4e529be1fe3e945e3b06350739574068b6fde53c
Kyle <47279470+unblinky@users.noreply.github.com>
2021-09-11 02:00:00 -0700

    Making progress with the Signpost.

--- Code/KirkjaOnline/Lawforms/Signpost/assets/signpost.gd
+++ Code/KirkjaOnline/Lawforms/Signpost/assets/signpost.gd
@@ -1,16 +1,22 @@
-#tool
+tool
 extends Spatial
 class_name Signpost
 
-export var lifetime: float = 4.0
+export var width: float = 1.024 setget set_width
+export var font_size: float = 3.3 setget set_font_size
 
-func Initialize(primary: Color, secondary: Color) -> void:
-	var material: Material = $Post.get_surface_material(0)
-	material.set_shader_param("Color", primary)
-	material = $Bulb.get_surface_material(0)
-	material.set_shader_param("Color", secondary)
+func set_width(value):
+	width = value
 	
-
-func _on_Timer_timeout() -> void:
-	queue_free()
-
+func set_font_size(value):
+	font_size = value
+	
+#------------------------------------------------------------------------------
+#func Initialize(primary: Color, secondary: Color) -> void:
+#	var material: Material = $Post.get_surface_material(0)
+#	material.set_shader_param("Color", primary)
+#	material = $Bulb.get_surface_material(0)
+#	material.set_shader_param("Color", secondary)
+	
+# TODO: Automatically size the width?
+# TODO: 
