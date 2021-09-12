4812658f403096d66187799e4b298f9815d55f39
Kyle <47279470+unblinky@users.noreply.github.com>
2021-09-11 23:39:21 -0700

    Finished Signpost base functionality, Spawner class that will allow multiple instances with timers.

--- Code/KirkjaOnline/Lawforms/Signpost/assets/signpost.gd
+++ Code/KirkjaOnline/Lawforms/Signpost/assets/signpost.gd
@@ -1,15 +1,41 @@
 tool
-extends Spatial
 class_name Signpost
+extends StaticBody
 
-export var width: float = 1.024 setget set_width
-export var font_size: float = 3.3 setget set_font_size
+export(String, MULTILINE) var message: String = "" setget set_message
+export(int, 25, 100) var font_size: int = 40 setget set_font_size
+export(Color, RGB) var font_color: Color = Color.green setget set_font_color
+#export(Color, RGB) var frame_color: Color = Color.whitesmoke setget set_frame_color
 
-func set_width(value):
-	width = value
-	
+#export var max_width: float = 1.6 setget set_max_width
+#export var max_height: float = 0.9 setget set_max_height
+
+
+onready var FONT: Font = $Viewport/Control/Panel/TextEdit.get_font("font")
+
+func set_message(value):
+	message = value
+	$Viewport/Control/Panel/TextEdit.text = message
+
 func set_font_size(value):
-	font_size = value
+	font_size = value	
+	#FONT.size = font_size
+
+func set_font_color(value):
+	font_color = value
+	var color: Color = $Viewport/Control/Panel/TextEdit/.get_color("font_color")
+	print(color)
+	
+#func set_frame_color(value):
+#
+#	pass
+	
+#func set_max_width(value):
+#	max_width = value
+#
+#func set_max_height(value):
+#	max_height = value
+	
 	
 #------------------------------------------------------------------------------
 #func Initialize(primary: Color, secondary: Color) -> void:
--- Code/KirkjaOnline/Main/assets/main.gd
+++ Code/KirkjaOnline/Main/assets/main.gd
@@ -1,15 +1,6 @@
 extends Node
 
-# Resources.
-const VOIDLING_PS: PackedScene = preload("res://Lawforms/Voidling/Voidling.tscn")
-const bubble_ps: PackedScene = preload("res://Lawforms/Bubble/Bubble.tscn")
-
-# Nodes.
 onready var VOID: Spatial = $Node3D/Void
-
-# Hey.
-func Hey(message: String) -> void:
-	print(message)
 
 
 #------------------------------------------------------------------------------
@@ -17,9 +8,6 @@ func Hey(message: String) -> void:
 #------------------------------------------------------------------------------
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	var voidling: Voidling = VOIDLING_PS.instance().Init(self, "Harvy / Nuts")
-	voidling.translation = Vector3.ZERO
-	VOID.add_child(voidling)
 	print("Ready")
 
 
@@ -33,12 +21,3 @@ func _unhandled_input(event):
 	if event is InputEventJoypadButton:
 		if event.pressed and event.button_index == 11: # Joypad [Start] button.
 			get_tree().quit()
-
-
-#------------------------------------------------------------------------------
-# Spawn bubble.
-#------------------------------------------------------------------------------
-func _on_Timer_timeout() -> void:
-	var bubble = bubble_ps.instance()
-	
-	VOID.add_child(bubble)
