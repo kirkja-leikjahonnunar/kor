affd525dc1f50595a76b0c521f2d231cceb37f4a
unblinky <47279470+unblinky@users.noreply.github.com>
2022-01-28 22:36:25 -0800

    Working towards a planet and point gravity setting.

--- Code/GameClient/Content/Lawforms/PlayerMovement/ThirdPersonPOV/ThirdPersonPOV.gd
+++ Code/GameClient/Content/Lawforms/PlayerMovement/ThirdPersonPOV/ThirdPersonPOV.gd
@@ -1,44 +1,55 @@
 extends KinematicBody
 
+const ONE_G = 9.807 # m/s²
 onready var PIVOT := $CameraPivot
+export var MouseSenesitivity = 0.005
 
-export var MouseSenesitivity = 0.01
-export var speed: float = 30 # m/s.
 var direction: Vector3 # 0.0 - 1.0
-var velocity: Vector3
-var gravity: Vector3
-var jump_impulse: Vector3
+var max_speed: float = 40.0
+var speed: float = 0.0
+var acceleration: float = 1 # meter / sec?
+
+var gravity: Vector3 = Vector3.DOWN * ONE_G # Can point toward other directions.
 
 func _ready():
-	gravity = Vector3.DOWN * 9.8
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _process(delta):
-	direction = Vector3.ZERO
+func _physics_process(delta):
+	MovementLoop(delta)
+
+	# TODO: Testing the movement.
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+		
 	
+func MovementLoop(delta_time):
+	direction = Vector3.ZERO
+
 	if Input.is_action_pressed("move_up"):
 		direction += -transform.basis.z
-		print("Up")
-		
 	if Input.is_action_pressed("move_down"):
 		direction += transform.basis.z
-		print("Down")
-		
 	if Input.is_action_pressed("move_left"):
 		direction += -transform.basis.x
-		print("Left")
-		
 	if Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
-		print("Right")
-	
-	if Input.is_action_just_pressed("jump"):
-		gravity *= -1
-		print("jump")
-		
+
+#	if Input.is_action_just_pressed("jump"):
+#		gravity *= -1
+#		print("jump")
+
+	if direction == Vector3.ZERO:
+		speed = 0
+	else:
+		speed += acceleration * delta_time
+		if speed > max_speed:
+			speed = max_speed
+
 	direction = direction.rotated(Vector3.UP, PIVOT.rotation.y).normalized()
-	velocity = speed * direction + gravity 
+	#print(direction)
+	
+	var velocity = speed * direction# + gravity 
 	move_and_slide(velocity, Vector3.UP)
 
 func _input(event):
