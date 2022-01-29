3d4da1db24c932849f74419926a54bf7a62bf767
unblinky <47279470+unblinky@users.noreply.github.com>
2022-01-29 10:10:07 -0800

    Gliding rebuilt.

--- Code/GameClient/Content/Lawforms/PlayerMovement/ThirdPersonPOV/ThirdPersonPOV.gd
+++ Code/GameClient/Content/Lawforms/PlayerMovement/ThirdPersonPOV/ThirdPersonPOV.gd
@@ -1,16 +1,24 @@
 extends KinematicBody
 
-const ONE_G = 9.807 # m/s²
+const ONE_G = 9.807 # m/s².
+const MY_G = 40
+
 onready var PIVOT := $CameraPivot
+onready var SPEED_UI := $UI/SpeedUI
+onready var VELOCITY_UI := $UI/VelocityUI
 
-export var MouseSenesitivity: float = 0.005
+export var MouseSenesitivity: float = 0.005 # TODO: Doc magic number.
+
+# m/s.
+export var MaxSpeed: float = 10.0
+export var MinSpeed: float = 0.1
 export var AccelerationRate: float = 0.666
-export var BreakDecay: float = 0.95 # Damp? Dammping?
-export var IsFlying: bool = true
+export var BreakDecay: float = 0.95 # Damping.
+export var IsGliding: bool = true # Use flying or sticky gravity. Magnetic stick?
 
-var velocity: Vector3 # Current velocity vector.
+var velocity: Vector3 # velocity =  vector.
 var thrust: Vector3 # Thrust vector from the user's keyboard, mouse, or gamepad.
-var gravity: Vector3 = Vector3.DOWN * ONE_G # Can point toward other directions.
+var gravity: Vector3 = Vector3.DOWN * MY_G # ONE_G # Gravity vector can point toward other directions.
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -25,6 +33,7 @@ func _physics_process(delta):
 		
 
 func MovementLoop(delta_time):
+	# Calculate the thrust
 	thrust = Vector3.ZERO
 
 	# TODO: Add gamepad support.
@@ -36,26 +45,39 @@ func MovementLoop(delta_time):
 		thrust += -transform.basis.x * AccelerationRate
 	if Input.is_action_pressed("move_right"):
 		thrust += transform.basis.x * AccelerationRate
-	
-	# Fly.	
-	if Input.is_action_pressed("move_up"):
-		thrust += -transform.basis.y * AccelerationRate
-	if Input.is_action_pressed("move_down"):
-		thrust += transform.basis.y * AccelerationRate
-	
-	if Input.is_action_pressed("jump"):
-		pass
 	if Input.is_action_pressed("break"):
 		velocity *= BreakDecay
-		
-	# Orient the thrust around the player's y axis, so the [wsad] isn't orientent to true north.
-	thrust = thrust.rotated(Vector3.UP, PIVOT.rotation.y).normalized()
-
 	
-#	if velocity.length() < 20:
+	if IsGliding:
+		if Input.is_action_pressed("move_up"):
+			thrust += -transform.basis.y * AccelerationRate
+		if Input.is_action_pressed("move_down"):
+			thrust += transform.basis.y * AccelerationRate
+	else:
+		if Input.is_action_pressed("jump"):
+			pass
+			
+		gravity *= delta_time #* gravity
+		velocity += gravity
+	
+	# Orient the thrust around the player's y axis, so the [wsad] controls are relative to the viewport.
+	thrust = thrust.rotated(Vector3.UP, PIVOT.rotation.y)#.normalized()
+
+	# Add player input.
 	velocity += thrust
-		
-	move_and_slide(velocity, Vector3.UP) # Includes delta_time.
+	
+	# Clamp the velocity's magnitude according to the speed properties.
+	if velocity.length() < MinSpeed:
+		velocity = Vector3.ZERO
+	elif velocity.length() > MaxSpeed:
+		velocity = velocity.normalized() * MaxSpeed
+			
+	move_and_slide(velocity, Vector3.UP) # Includes thrust delta_time.
+	
+	# UI.
+	var speed = velocity.length()
+	SPEED_UI.text = "Speed: %s" % speed
+	VELOCITY_UI.text = "Velocity: %s" % velocity
 
 func _input(event):
 	if event is InputEventMouseMotion:
