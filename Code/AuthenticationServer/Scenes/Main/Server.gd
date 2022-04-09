4d010186e32f8817857703c923eadcd36fb694cf
tomlechner <tom@tomlechner.com>
2022-04-09 14:00:26 -0700

    Rpc function duplication snafus sort of cleared

--- Code/AuthenticationServer/Scenes/Main/Server.gd
+++ Code/AuthenticationServer/Scenes/Main/Server.gd
@@ -21,6 +21,8 @@ func start_server():
 	#network.connect("peer_connected", peer_connected, [])
 	network.peer_connected.connect(peer_connected)
 	network.peer_disconnected.connect(peer_disconnected)
+	
+	#print("peer id: "+network.)
 
 
 func peer_connected(player_id):
@@ -29,4 +31,26 @@ func peer_connected(player_id):
 
 func peer_disconnected(player_id):
 	print ("User " + str(player_id) + " Disconnected!")
+
+
+#@rpc(any_peer)
+#func RequestPlayerData(what:String, requestor:int):
+#	var player_id = multiplayer.get_remote_sender_id()
+#	print("RequestPlayerData: ", what, ": ", requestor)
+#
+#	print("sending response for "+what)
+#	rpc_id(player_id, "PlayerDataResponse", what+"-booyah", requestor)
+
+
+
+@rpc(any_peer)
+func RequestPlayerData():
+	var player_id = multiplayer.get_remote_sender_id()
+	print("RequestPlayerData on server")
+	
+	rpc_id(player_id, "PlayerDataResponse")
+
+
+# this is a stub, the real function is on client side
+@rpc(any_peer) func PlayerDataResponse(): pass
 
--- Code/GameClient/Scenes/Singletons/Server.gd
+++ Code/GameClient/Scenes/Singletons/Server.gd
@@ -25,3 +25,42 @@ func connection_failed():
 
 func connection_succeeded():
 	print ("Connection succeeded!")
+	RequestPlayerDataLocal()
+	#RequestPlayerData("thing", get_instance_id())
+
+
+#@rpc
+#func RequestPlayerData(what: String, requestor: int):
+#	print("RequestPlayerData ",what," requestor: ", requestor)
+#
+#	rpc_id(1, "RequestPlayerData", what, requestor)
+#
+#
+#@rpc(any_peer)
+#func PlayerDataResponse(returned, requestor):
+#	print("client received response: "+returned)
+#	#instance_from_id(requestor).dealwithit
+
+
+func RequestPlayerDataLocal():
+	print("RequestPlayerData on client")
+	
+	rpc_id(1, "RequestPlayerData")
+
+
+# this is a stub, the true function is on the real server
+@rpc(any_peer)
+func RequestPlayerData():
+	pass
+
+
+@rpc(any_peer)
+func PlayerDataResponse():
+	print("client received response: ")
+	#instance_from_id(requestor).dealwithit
+
+
+#@rpc(unreliable)
+#func UpdateWhatever():
+#	pass
+
