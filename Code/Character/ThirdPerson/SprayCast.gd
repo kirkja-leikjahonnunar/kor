0dd4e4782af187b7488e5e72d4b3aea9e8a96afd
Tom Lechner <tom@tomlechner.com>
2022-04-26 00:32:23 -0700

    Work on movement on funky surface

--- Code/Character/ThirdPerson/SprayCast.gd
+++ Code/Character/ThirdPerson/SprayCast.gd
@@ -3,6 +3,9 @@ extends RayCast3D
 @export var spray_image : Resource
 @export var MainCamera : NodePath
 
+@onready var main_camera = get_node(MainCamera) if !MainCamera.is_empty() else null
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,8 +15,28 @@ func _ready():
 func _process(_delta):
 	#print(is_colliding())
 	
+	# make sprayer point at position currently gazed at
+	if main_camera != null:
+		var gaze : RayCast3D = main_camera.get_node("SelectorCast")
+		if gaze == null:
+			printerr("missing gaze raycaster on main camera")
+		else:
+			if gaze.is_colliding():
+				var hit_point = gaze.get_collision_point()
+				var new_tr = AlignTransformY(global_transform.origin, hit_point, global_transform, Vector3(0,1,0))
+				global_transform = new_tr
+	
 	if is_colliding() && Input.is_action_just_pressed("char_use1"):
 		Spray()
+
+
+# return a transform with origin at from_point that has its Y axis toward to_point
+func AlignTransformY(from_point: Vector3, to_point: Vector3, 
+					tr: Transform3D, up: Vector3) -> Transform3D:
+	var new_y := -(to_point - from_point).normalized()
+	var new_x := new_y.cross(up)
+	var new_z := new_x.cross(new_y)
+	return Transform3D(new_x, new_y, new_z, from_point)
 
 
 func Spray():
